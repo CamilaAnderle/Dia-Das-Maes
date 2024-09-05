fc866c7882408789401b1c0a89c2a1acac7020e1
CamilaAnderle <camila.anderle.souza.0111@gmail.com>
2024-09-05 10:31:46 -0300

    Primeiro commit

---
+++ obrigado.php
@@ -0,0 +1,135 @@
+
+<!doctype html>
+
+<html class="no-js" lang="en">
+
+
+
+<head>
+
+    <title>Obrigado! | Raabe Luz</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="Wease | wease.com.br">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+    <meta name="description" content="Venda sem aparecer, sem gastar com anúncios e usar a sua voz.">
+    <meta name="keywords" content="vídeo review, Raabe Luz, faturar, dinheiro, moedas, curso online">
+    <meta data-n-head="ssr" data-hid="og-type" property="og:type" content="website">
+    <meta data-n-head="ssr" data-hid="og-title" property="og:title" content="Obrigado | Raabe Luz">
+    <meta data-n-head="ssr" data-hid="og-desc" property="og:description" content="Venda sem aparecer, sem gastar com anúncios e usar a sua voz.">
+    <!-- favicon icon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+    <!-- style sheets and font icons  -->
+    <link rel="stylesheet" type="text/css" href="css/font-icons.min.css">
+    <link rel="stylesheet" type="text/css" href="css/theme-vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" />
+    <!-- revolution slider -->
+    <link rel='stylesheet' href="revolution/revolution-addons/bubblemorph/css/revolution.addon.bubblemorph.css" type='text/css' media='all' />
+    <link rel="stylesheet" type="text/css" href="revolution/css/settings.css">
+    <link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+    <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css">
+
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1229031731336097');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1229031731336097&ev=PageView&noscript=1" /></noscript>
+    <!-- End Meta Pixel Code -->
+
+</head>
+
+<body data-mobile-nav-style="classic">
+    <!-- start section -->
+    <section class="bg-white overflow-visible pt-3 position-relative" style="background-image: url(images/bg-inicio.jpg);background-size:cover;">
+        <div class="container">
+            <div class="w-100">
+                <img src="images/Youtube.png" class="float-image" style="left: 0;">
+                <img src="images/coin.png" class="float-image" style="right: 0;">
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-12 col-xl-7 col-lg-8 col-md-9 d-flex flex-column justify-content-center text-center margin-8-half-rem-top margin-5-half-rem-bottom md-margin-4-half-rem-bottom md-margin-7-rem-top wow animate__fadeIn">
+                    <h4 class="alt-font font-weight-700 w-100 sm-w-100 md-margin-auto-lr sm-margin-auto-lr purple-prince margin-10px-bottom sm-w-100 sm-margin-15px-bottom">Você está quase lá…</h4>
+                    <!-- start progress bar item -->
+                    <div class="progress progress-step-style-01 margin-4-half-rem-bottom sm-margin-6-rem-bottom mb-5 d-flex justify-content-center">
+                        <div class="progress-bar bg-blue-purple" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"><span class="alt-font text-small text-extra-dark-gray font-weight-500">95%</span></div>
+                    </div>
+                    <!-- end progress bar item -->
+                    <iframe class="m-auto mb-5 w-90" width="560" height="315" src="https://www.youtube.com/embed/dz09fJ_Yy6A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <a class="btn mt-3 btn-purple-prince mb-3 blob" target="_blank" href="https://chat.whatsapp.com/EdH08pkFBWg1ovYvwQhCjZ">Entrar no grupo agora!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end section -->
+
+    <!-- start footer -->
+    <footer class="footer-decor footer-dark bg-purple-prince">
+        <div class="footer-top padding-five-tb lg-padding-eight-tb md-padding-50px-tb">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <!-- start footer column -->
+                    <div class="col-12 col-lg-10 text-center last-paragraph-no-margin md-margin-40px-bottom xs-margin-25px-bottom">
+                        <p class="w-100">AVISO LEGAL</p>
+                        <p class="w-100">Nenhuma informação contida neste site deve ser interpretada como uma afirmação da obtenção de resultados. Qualquer referência ao desempenho passado ou potencial de uma estratégia abordada no conteúdo do mesmo, não é, nem deve ser interpretada como uma garantia de qualquer resultado. </p>
+                    </div>
+                    <!-- end footer column -->
+                    <!-- start footer column -->
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom padding-40px-tb border-top border-color-white-transparent">
+            <div class="container">
+                <div class="row align-items-center justify-content-center">
+                    <div class="col-12 col-md-6 text-center last-paragraph-no-margin sm-margin-20px-bottom">
+                        <p>&copy; 2024 Proudly Created by <a href="https://www.wease.com.br/" target="_blank">Wease</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- end footer -->
+
+    <!-- start scroll to top -->
+    <a class="scroll-top-arrow" href="javascript:void(0);"><i class="feather icon-feather-arrow-up"></i></a>
+    <!-- end scroll to top -->
+
+    <!-- javascript -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/theme-vendors.min.js"></script>
+    <script type="text/javascript" src="vendor/jquery-mask/jquery.mask.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+
+</html>
